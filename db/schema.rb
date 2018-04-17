7ed20e692de90b720c8e65abd9c7fd430798f0d6
SantaKruse <dkruse22@live.co.uk>
2018-04-17 22:27:23 +0100

    user_id added to events table and associations set for host

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416211136) do
+ActiveRecord::Schema.define(version: 20180417211353) do
 
   create_table "events", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.date     "date"
     t.time     "time"
     t.string   "location"
     t.string   "theme"
-    t.integer  "host_user_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
